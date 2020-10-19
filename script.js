7ab727e4c1166658f6ae33d012ff97c1f5702c4b
wubuchako <56101474+wubuchako@users.noreply.github.com>
2020-10-20 00:22:06 +0900

    Drag and Drop items

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const saveItemBtns = document.querySelectorAll('.solid');
 const addItemContainers = document.querySelectorAll('.add-container');
 const addItems = document.querySelectorAll('.add-item');
 // Item Lists
-const itemLists = document.querySelectorAll('.drag-item-list');
+const listColumns = document.querySelectorAll('.drag-item-list');
 const backlogList = document.getElementById('backlog-list');
 const progressList = document.getElementById('progress-list');
 const completeList = document.getElementById('complete-list');
@@ -21,6 +21,7 @@ let listArrays = [];
 
 // Drag Functionality
 let draggedItem;
+let currentColumn;
 
 // Get Arrays from localStorage if available, set default values if not
 function getSavedColumns() {
@@ -106,9 +107,22 @@ function allowDrop(e) {
   e.preventDefault();
 }
 
+// When Item Enters column area
+function dragEnter(column){
+  listColumns[column].classList.add('over');
+  currentColumn = column;
+}
+
 // Dropping Item in Column
 function drop(e){
   e.preventDefault();
+  // Remove background color/padding
+  listColumns.forEach((column) => {
+    column.classList.remove('over');
+  }); 
+ // Add Item to column
+ const parent = listColumns[currentColumn];
+ parent.appendChild(draggedItem);
 }
 
 // On load
